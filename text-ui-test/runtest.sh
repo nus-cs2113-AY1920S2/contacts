b08e953b4442f5db0ad3806abd2b96266db164b6
lowxizhi <lowxizhi@gmail.com>
2020-01-29 13:06:09 +0800

    extract method and set constant in contacts0.java

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,14 +13,14 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/Contact0.java
+if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/Contacts0.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Contact0 < input.txt > ACTUAL.TXT
+java -classpath ../bin Contacts0 < input.txt > ACTUAL.TXT
 
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED.TXT
