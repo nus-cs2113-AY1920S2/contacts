ef38fb56fcba787dcbfb8d2ab94169e709207732
Samantha Goh <>
2020-01-29 13:20:49 +0800

    changes

--- src/main/java/Contacts0.java
+++ src/main/java/Contacts0.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class Contacts0 {
 
+    public static final int INT = 100;
+
     public static void main(String[] args) {
         final Scanner SCANNER = new Scanner(System.in);
         System.out.println("|| ===================================================");
@@ -10,7 +12,7 @@ public class Contacts0 {
         System.out.println("|| Contacts - Version 0.0");
         System.out.println("|| Welcome to Contacts!");
         System.out.println("|| ===================================================");
-        String[][] list = new String[100][3];
+        String[][] list = new String[INT][3];
         int count = 0;
         while (true) {
             System.out.print("|| " + "Enter command: ");
@@ -92,7 +94,7 @@ public class Contacts0 {
                 feedback = String.format("%1$d persons found!", count);
                 break;
             case "clear":
-                list = new String[100][3];
+                list = new String[INT][3];
                 count = 0;
                 feedback = "Contacts have been cleared!";
                 break;
