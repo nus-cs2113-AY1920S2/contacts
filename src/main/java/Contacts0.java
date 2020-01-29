8d29e13d0bd63bcf1def2651f16bc64c2a0506bb
Alaukik <alaukik.pant@u.yale-nus.edu.sg>
2020-01-29 12:50:50 +0800

    added

--- src/main/java/Contacts0.java
+++ src/main/java/Contacts0.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 
 public class Contacts0 {
 
+    public static final int INT = 100;
+
     public static void main(String[] args) {
         final Scanner SCANNER = new Scanner(System.in);
         System.out.println("|| ===================================================");
         System.out.println("|| ===================================================");
-        System.out.println("|| Contacts - Version 0.0");
+        System.out.println("|| Contacts - Version 0.0----");
         System.out.println("|| Welcome to Contacts!");
         System.out.println("|| ===================================================");
-        String[][] list = new String[100][3];
+        String[][] list = new String[INT][3];
         int count = 0;
         while (true) {
             System.out.print("|| " + "Enter command: ");
