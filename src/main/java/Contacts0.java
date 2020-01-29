d335a62996e5adf5cd92e3f88dc1d8200cf164d5
jiajuinphoon <phoonjiajuin@hotmail.com>
2020-01-29 13:24:19 +0800

    small change at contacts 0

--- src/main/java/Contacts0.java
+++ src/main/java/Contacts0.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 
 public class Contacts0 {
 
+    public static final int MAXIMUM_CAPACITY = 100;
+
     public static void main(String[] args) {
         final Scanner SCANNER = new Scanner(System.in);
-        System.out.println("|| ===================================================");
-        System.out.println("|| ===================================================");
-        System.out.println("|| Contacts - Version 0.0");
-        System.out.println("|| Welcome to Contacts!");
-        System.out.println("|| ===================================================");
-        String[][] list = new String[100][3];
+        displayWelcomeMessage();
+        String[][] list = new String[MAXIMUM_CAPACITY][3];
         int count = 0;
         while (true) {
             System.out.print("|| " + "Enter command: ");
@@ -92,7 +90,7 @@ public class Contacts0 {
                 feedback = String.format("%1$d persons found!", count);
                 break;
             case "clear":
-                list = new String[100][3];
+                list = new String[MAXIMUM_CAPACITY][3];
                 count = 0;
                 feedback = "Contacts have been cleared!";
                 break;
@@ -144,6 +142,14 @@ public class Contacts0 {
                 System.out.println("|| " + m);
             }
         }
+    }
+
+    private static void displayWelcomeMessage() {
+        System.out.println("|| ===================================================");
+        System.out.println("|| ===================================================");
+        System.out.println("|| Contacts - Version 0.0");
+        System.out.println("|| Welcome to Contacts!");
+        System.out.println("|| ===================================================");
     }
 
 }
