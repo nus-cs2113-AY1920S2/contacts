5c42877c6381265cd4a3e06ffd702632e11282c5
Yolanda <yolanda993322@gmail.com>
2020-02-02 10:14:50 -0500

    Refactor messages and change typos

--- src/main/java/Contacts0.java
+++ src/main/java/Contacts0.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 
 
 public class Contacts0 {
+    //constants have a different naming convention
+    public static final int MAX_CAPACITY = 100;
 
     public static void main(String[] args) {
         final Scanner SCANNER = new Scanner(System.in);
-        System.out.println("|| ===================================================");
-        System.out.println("|| ===================================================");
-        System.out.println("|| Contacts - Version 0.0");
-        System.out.println("|| Welcome to Contacts!");
-        System.out.println("|| ===================================================");
-        String[][] list = new String[100][3];
+        displayWelcomeMessage();
+        String[][] list = new String[MAX_CAPACITY][3];
         int count = 0;
         while (true) {
             System.out.print("|| " + "Enter command: ");
@@ -144,6 +142,14 @@ public class Contacts0 {
                 System.out.println("|| " + m);
             }
         }
+    }
+
+    private static void displayWelcomeMessage() {
+        System.out.println("|| ===================================================");
+        System.out.println("|| ===================================================");
+        System.out.println("|| Contacts - Version 0.0");
+        System.out.println("|| Welcome to Contacts!");
+        System.out.println("|| ===================================================");
     }
 
 }
