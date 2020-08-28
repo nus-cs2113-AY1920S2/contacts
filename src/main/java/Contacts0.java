378ef4f0bd3c7f21e0e8f73c46fc0d47493b0fdf
R Ramana <e0398581@u.nus.edu>
2020-08-28 17:41:13 +0800

    Refactor code

--- src/main/java/Contacts0.java
+++ src/main/java/Contacts0.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 
 public class Contacts0 {
 
+    public static final int MAX_CAPACITY = 100;
+
     public static void main(String[] args) {
         final Scanner SCANNER = new Scanner(System.in);
-        System.out.println("|| ===================================================");
-        System.out.println("|| ===================================================");
-        System.out.println("|| Contacts - Version 0.0");
-        System.out.println("|| Welcome to Contacts!");
-        System.out.println("|| ===================================================");
-        String[][] list = new String[100][3];
+        showWelcomeScreen();
+        String[][] list = new String[MAX_CAPACITY][3];
         int count = 0;
         while (true) {
             System.out.print("|| " + "Enter command: ");
@@ -144,6 +142,14 @@ public class Contacts0 {
                 System.out.println("|| " + m);
             }
         }
+    }
+
+    private static void showWelcomeScreen() {
+        System.out.println("|| ===================================================");
+        System.out.println("|| ===================================================");
+        System.out.println("|| Contacts - Version 0.0");
+        System.out.println("|| Welcome to Contacts!");
+        System.out.println("|| ===================================================");
     }
 
 }
